92d41797f96cd80c2c3acb2a4b90741c4865ee74
chiuanwei <weizhuquan@vip.qq.com>
2016-01-26 00:21:36 +0800

    - add upload project 2016-1-26

---
+++ TTConsole.Unity/Assets/Code/TTDebuger.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace TinyTeam.Debuger
+{
+    /// <summary>
+    /// Description： custom debuger,can control debug or record in memory writing in thread.
+    /// Author： chiuanwei
+    /// CreateTime: 2015-2-12
+    /// </summary>
+
+	
+    public class TTDebuger
+    {
+        static public bool EnableLog = true;
+        static public void Log(object message)
+        {
+            Log(message, null);
+        }
+
+        static public void Log(object message, string customType)
+        {
+            if (EnableLog)
+            {
+                Console.Log(message,customType);
+            }
+        }
+
+        static public void Log(object message, string customType,Color col)
+        {
+            if (EnableLog)
+            {
+                Console.Log(message, customType, col);
+            }
+        }
+
+        static public void LogError(object message)
+        {
+            LogError(message, null);
+        }
+        static public void LogError(object message, string customType)
+        {
+            if (EnableLog)
+            {
+                Console.LogError(message, customType);
+            }
+        }
+        static public void LogWarning(object message)
+        {
+            LogWarning(message, null);
+        }
+        static public void LogWarning(object message, string customType)
+        {
+            if (EnableLog)
+            {
+                Console.LogWarning(message, customType);
+            }
+        }
+
+        public static void RegisterCommand(string commandString, Func<string[],object> commandCallback, string CMD_Discribes)
+        {
+            Console.RegisterCommand(commandString, commandCallback, CMD_Discribes);
+        }
+
+        public static void UnRegisterCommand(string commandString)
+        {
+            Console.UnRegisterCommand(commandString);
+        }
+
+    }
+}
---
+++ TTConsole.Unity/Assets/NewBehaviourScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using TinyTeam.Debuger;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public int max = 10;
+    bool start = false;
+    public GameObject go = null;
+
+	// Use this for initialization
+	void Start () {
+        TTDebuger.Log("Hello Chiuan");
+        TTDebuger.Log("Hello Chiuan","",new Color(1,0.3f,1));
+
+        //EventTrigger tr = go.AddComponent<EventTrigger>();
+        //EventTrigger.Entry en = new EventTrigger.Entry();
+
+        ////EventTrigger.TriggerEvent te
+        ////en.callback = 
+        //tr.delegates = new System.Collections.Generic.List<EventTrigger.Entry>();
+        //tr.delegates.Add(en);
+
+        //TTDebuger.RegisterCommand("load", LoadLevel, "");
+        //StartCoroutine(IEDebug());
+
+        //TTDebuger.Log("hello man");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnGUI()
+    {
+        GUILayout.Label("Press ~ button to open the console in destop. 3 touches upside screen in mobiles.");
+    }
+
+    //object LoadLevel(string[] input)
+    //{
+    //    Console.IsOpen = false;
+    //    Application.LoadLevel(0);
+    //    return "load level";
+    //}
+
+    IEnumerator IEDebug()
+    {
+        int i = 0;
+        while(true)
+        {
+            yield return null;
+            if (i == max) break;
+            i++;
+            TTDebuger.Log(i + " [Add] cached assetbundle :comm.texture.xulie-0118.png.assetbundle[Add] cached assetbundle :comm.texture.xulie-0118.png.assetbundle[Add] cached assetbundle :comm.texture.xulie-0118.png.assetbundlecomm.texture.xulie-0118.png.assetbundlecomm.texture.xulie-0118.png.assetbundlecomm.texture.xulie-0118.png.assetbundlecomm.texture.xulie-0118.png.assetbundlecomm.texture.xulie-0118.png.assetbundle");
+            //Debug.Log("gogogogo");
+        }
+
+        start = !start;
+    }
+
+    public void OnSubmit(InputField input)
+    {
+        Debug.Log(input.text);
+    }
+}
