cb2d2bfc93a5c8852edeea8061f495e682e6f417
leviyuan <450446856@qq.com>
2017-07-06 20:02:54 +0800

    - add debug buttons

--- TTConsole_Source_Unity/Assets/NewBehaviourScript.cs
+++ TTConsole_Source_Unity/Assets/NewBehaviourScript.cs
@@ -10,67 +10,78 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour {
-   public int max = 10;
-   bool start = false;
-   public GameObject go = null;
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int max = 10;
+    bool start = false;
+    public GameObject go = null;
 
-   // Use this for initialization
-   void Start () {
-      TTDebuger.EnableLog = true;
+    // Use this for initialization
+    void Start()
+    {
+        TTDebuger.EnableLog = true;
 
-      TTDebuger.Log ("Hello Chiuan");
-      TTDebuger.Log ("socket...", "NET");
-      TTDebuger.Log ("load xxxx.ab", "Loader");
-      TTDebuger.Log ("success mission", "mission");
+        TTDebuger.Log("Hello Chiuan");
+        TTDebuger.Log("socket...", "NET");
+        TTDebuger.Log("load xxxx.ab", "Loader");
+        TTDebuger.Log("success mission", "mission");
 
-      //EventTrigger tr = go.AddComponent<EventTrigger>();
-      //EventTrigger.Entry en = new EventTrigger.Entry();
+        //EventTrigger tr = go.AddComponent<EventTrigger>();
+        //EventTrigger.Entry en = new EventTrigger.Entry();
 
-      //tr.delegates = new System.Collections.Generic.List<EventTrigger.Entry>();
-      //tr.delegates.Add(en);
+        //tr.delegates = new System.Collections.Generic.List<EventTrigger.Entry>();
+        //tr.delegates.Add(en);
 
-      //TTDebuger.RegisterCommand("load", LoadLevel, "");
-      // StartCoroutine(IEDebug());
+        //TTDebuger.RegisterCommand("load", LoadLevel, "");
+        // StartCoroutine(IEDebug());
 
-      //TTDebuger.Log("hello man");
-   }
+        //TTDebuger.Log("hello man");
 
-   // Update is called once per frame
-   void Update () {
+        for (int i = 0; i < 10; i++)
+            TTDebuger.RegisterButton("btn" + i.ToString(), "net " + i);
+    }
 
-   }
+    // Update is called once per frame
+    void Update()
+    {
 
-   void OnGUI () {
-      GUILayout.Label ("Press ~ button to open the console in destop. 3 touches upside screen in mobiles.");
+    }
 
-      GUILayout.Space (20);
-      if (GUILayout.Button ("logging")) {
-         TTDebuger.Log ("socket...", "NET");
-      }
-   }
+    void OnGUI()
+    {
+        GUILayout.Label("Press ~ button to open the console in destop. 3 touches upside screen in mobiles.");
 
-   //object LoadLevel(string[] input)
-   //{
-   //    Console.IsOpen = false;
-   //    Application.LoadLevel(0);
-   //    return "load level";
-   //}
+        GUILayout.Space(20);
+        if (GUILayout.Button("logging"))
+        {
+            TTDebuger.Log("socket...", "NET");
+        }
+    }
 
-   IEnumerator IEDebug () {
-      int i = 0;
-      while (true) {
-         yield return null;
-         if (i == max) break;
-         i++;
-         TTDebuger.Log (i + " [Add] cached assetbundle :comm.texture.xulie-0118.png.assetbundle[Add] cached assetbundle :comm.texture.xulie-0118.png.assetbundle[Add] cached assetbundle :comm.texture.xulie-0118.png.assetbundlecomm.texture.xulie-0118.png.assetbundlecomm.texture.xulie-0118.png.assetbundlecomm.texture.xulie-0118.png.assetbundlecomm.texture.xulie-0118.png.assetbundlecomm.texture.xulie-0118.png.assetbundle");
-         //Debug.Log("gogogogo");
-      }
+    //object LoadLevel(string[] input)
+    //{
+    //    Console.IsOpen = false;
+    //    Application.LoadLevel(0);
+    //    return "load level";
+    //}
 
-      start = !start;
-   }
+    IEnumerator IEDebug()
+    {
+        int i = 0;
+        while (true)
+        {
+            yield return null;
+            if (i == max) break;
+            i++;
+            TTDebuger.Log(i + " [Add] cached assetbundle :comm.texture.xulie-0118.png.assetbundle[Add] cached assetbundle :comm.texture.xulie-0118.png.assetbundle[Add] cached assetbundle :comm.texture.xulie-0118.png.assetbundlecomm.texture.xulie-0118.png.assetbundlecomm.texture.xulie-0118.png.assetbundlecomm.texture.xulie-0118.png.assetbundlecomm.texture.xulie-0118.png.assetbundlecomm.texture.xulie-0118.png.assetbundle");
+            //Debug.Log("gogogogo");
+        }
 
-   public void OnSubmit (InputField input) {
-      Debug.Log (input.text);
-   }
+        start = !start;
+    }
+
+    public void OnSubmit(InputField input)
+    {
+        Debug.Log(input.text);
+    }
 }
